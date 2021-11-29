77230fe39dc41d9dec4f66e9e51a80bf364a4999
Dat Tran <dat.tran@ghielectronics.com>
2021-11-29 12:01:08 -0500

    change sc13xxx firmware to 6400

--- src/App.vue
+++ src/App.vue
@@ -139,9 +139,9 @@ export default {
                     isGlb: true,
                     image: null,
                 },
-                'TinyCLR_2_1_0_6300': {
-                    title: '.NET C# (TinyCLR OS v2.1.0.6300)',
-                    url: 'firmware/SITCore-SC13-Firmware-v2.1.0.6300.ghi',
+                'TinyCLR_2_1_0_6400': {
+                    title: '.NET C# (TinyCLR OS v2.1.0.6400)',
+                    url: 'firmware/SITCore-SC13-Firmware-v2.1.0.6400.ghi',
                     isGlb: false,
                     image: null,
                 },
