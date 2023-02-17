42068480dd9748ade4e8483bc9afef8ce29196e9
Dat Tran <dat.tran@ghielectronics.com>
2023-02-17 16:02:11 -0500

    increase due v0.0.3

--- src/App.vue
+++ src/App.vue
@@ -151,9 +151,9 @@ export default {
                     isGlb: false,
                     image: null,
                 },
-				'BrainPad_Experimental_0_0_2': {
-                    title: 'BrainPad_Experimental (v0.0.2)',
-                    url: 'firmware/Brainpad_experimental_002.ghi',
+				'BrainPad_Experimental_0_0_3': {
+                    title: 'BrainPad_Experimental (v0.0.3)',
+                    url: 'firmware/Brainpad_experimental_003.ghi',
                     isGlb: false,
                     image: null,
                 },
