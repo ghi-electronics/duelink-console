9cbb9b0424c19e95cbc09ac5f4662795d888f687
Dat Tran <dat.tran@ghielectronics.com>
2023-05-19 14:08:33 -0400

    to firmware 1.0.1

--- src/js/firmware.js
+++ src/js/firmware.js
@@ -2,7 +2,7 @@ export default {
     'Due_SC13': {
         name: 'SC13',
         versions: [
-            { id: 1, name: 'v1.0.0', url: 'firmware/due_sc13_v100.ghi' },
+            { id: 1, name: 'v1.0.1', url: 'firmware/due_sc13_v101.ghi' },
         ],
         boards: [
             { name: 'FEZ Flea', id: 'F' },
@@ -13,7 +13,7 @@ export default {
     'Due_SC007': {
         name: 'SC007',
         versions: [
-            { id: 1, name: 'v1.0.0', url: 'firmware/due_sc007_v100.ghi' },
+            { id: 1, name: 'v1.0.1', url: 'firmware/due_sc007_v101.ghi' },
         ],
         boards: [
             { name: 'BrainPad Edge', id: 'E' },
