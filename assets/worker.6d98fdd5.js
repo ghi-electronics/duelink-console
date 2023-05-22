b2f93d91f8a7db79980984cf0896166724183956
dudedigital <dudedigital@users.noreply.github.com>
2023-05-22 22:15:43 +0000

    deploy: 7b449306ec1efa37b54e96d3bd2f3f5f844b5b54

--- assets/worker.6d98fdd5.js
+++ assets/worker.6d98fdd5.js
@@ -1,5 +1,69 @@
 console.log('worker');
-importScripts(new URL('ConsumerQueue.js', import.meta.url));
+
+class ConsumerQueue {
+    constructor() {
+        this._head = null;
+        this._tail = null;
+        this._waiter = null;
+        this._cancel = null;
+    }
+
+    tryPop() {
+        if (this._head !== null) {
+            let data = this._head.data;
+            this._head = this._head.next;
+            if (this._head === null)
+                this._tail = null;
+            return data;
+        } else {
+            return null;
+        }
+    }
+
+    // For compatibility with AsyncIterator
+    next() {
+        return this.pop();
+    }
+
+    pop() {
+        if (this._head !== null) {
+            return Promise.resolve(this.tryPop());
+        } else if (this._waiter !== null) {
+            throw new Error('Someone is already waiting on this queue');
+        } else {
+            return new Promise((callback, errback) => {
+                this._waiter = callback;
+                this._cancel = errback;
+            });
+        }
+    }
+
+    cancelWait(err) {
+        let cancel = this._cancel;
+        this._cancel = null;
+        this._waiter = null;
+        if (cancel)
+            cancel(err);
+    }
+
+    hasWaiter() {
+        return this._waiter !== null;
+    }
+
+    push(data) {
+        let waiter = this._waiter;
+        this._waiter = null;
+        this._cancel = null;
+        if (waiter) {
+            waiter(data);
+        } else if (this._tail === null) {
+            this._head = this._tail = { data: data, next: null };
+        } else {
+            this._tail.next = { data: data, next: null };
+            this._tail = this._tail.next;
+        }
+    }
+}
 
 const decoder = new TextDecoder();
 const encoder = new TextEncoder();
