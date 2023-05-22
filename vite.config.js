efe3f744bb7d3107e3f0c285b9d2e6566c3bce04
dudedigital <dudedigital@yahoo.com>
2023-05-22 19:33:50 -0400

    Updates.

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,15 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
     base: './', // Make assets relative
-    // build: {
-    //     minify: 'terser',
-    //     terserOptions: {
-    //         compress: {
-    //             drop_console: true,
-    //             drop_debugger: true,
-    //         },
-    //     },
-    // },
+    build: {
+        minify: 'terser',
+        terserOptions: {
+            compress: {
+                drop_console: true,
+                drop_debugger: true,
+            },
+        },
+    },
     plugins: [
         vue(),
     ],
