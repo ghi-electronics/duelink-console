d534a5256bd2640c04766e368e28358b11ab5df5
dudedigital <dudedigital@yahoo.com>
2023-08-11 11:32:50 -0400

    Move not supported to connect.

--- src/js/useWebSerial.js
+++ src/js/useWebSerial.js
@@ -34,6 +34,10 @@ export default function useWebSerial($refs) {
     // Methods - Toolbar
 
     async function connect() {
+        if (window.document.documentMode || !navigator.serial) {
+            window.location = '/browser-not-supported.html';
+        }
+
         isBusy.value = true;
 
         try {
