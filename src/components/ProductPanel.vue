9fe6f50bfb4c134e0f6881e4ee29ccea7ecbdfdb
Dat Tran <dat.tran@ghielectronics.com>
2026-02-13 14:53:01 -0500

    add dark mode

--- src/components/ProductPanel.vue
+++ src/components/ProductPanel.vue
@@ -1,6 +1,9 @@
 <template>
     <!-- Hide panel completely if no product link -->
-    <div v-if="productLink" class="bg-white shadow rounded p-3">
+    <div v-if="productLink" class="bg-white dark:bg-gray-800 
+         border border-gray-200 dark:border-gray-700
+         shadow-sm rounded p-3 text-center
+         text-gray-800 dark:text-gray-200">
         <div class="flex justify-center mt-2">
             <img :src="productLink" alt="Product Image" class="max-w-full max-h-64 object-contain rounded"
                 @error="onImageError" />
@@ -12,12 +15,9 @@
         </div>
 
     </div>
-        <div class="flex justify-end px-4">
-        <a
-            class="mt-4 inline-block w-full sm:w-auto text-center sm:text-left px-4 py-2 font-medium rounded transition bg-slate-300 hover:bg-slate-500 text-slate-600 hover:text-slate-50 dark:bg-zinc-600 dark:hover:bg-zinc-300 dark:text-zinc-100 dark:hover:text-zinc-950"
-            href="https://github.com/ghi-electronics/due-console/issues/"
-            target="_blank"
-        >
+    <div class="flex justify-end px-4">
+        <a class="mt-4 inline-block w-full sm:w-auto text-center sm:text-left px-4 py-2 font-medium rounded transition bg-slate-300 hover:bg-slate-500 text-slate-600 hover:text-slate-50 dark:bg-zinc-600 dark:hover:bg-zinc-300 dark:text-zinc-100 dark:hover:text-zinc-950"
+            href="https://github.com/ghi-electronics/due-console/issues/" target="_blank">
             <i class="fas fa-fw fa-arrow-up-right-from-square mr-1"></i> Report Issue
         </a>
     </div>
